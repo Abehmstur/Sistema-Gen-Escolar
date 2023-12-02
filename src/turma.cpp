c6654e96cf4b28525f9588fc199c67f6dbe78bb1
Abehmstur <matheus.medeiros970@gmail.com>
2023-12-02 13:57:25 -0300

    Nova Classe Turma com encapsulamentos e getters and setters.

---
+++ src/turma.cpp
@@ -0,0 +1,35 @@
+#include "Turma.hpp"
+#include <iostream>
+
+// Construtor padrão
+Turma::Turma() : capacidadeAlunos(0) {}
+
+// Construtor parametrizado
+Turma::Turma(const std::string& _nomeDaTurma, Professor& _professorDaTurma, int _capacidadeAlunos)
+    : nomeDaTurma(_nomeDaTurma), professorDaTurma(_professorDaTurma), capacidadeAlunos(_capacidadeAlunos) {}
+
+// Getters
+std::string Turma::getNomeDaTurma() const {
+    return nomeDaTurma;
+}
+
+int Turma::getCapacidadeAlunos() const {
+    return capacidadeAlunos;
+}
+
+Professor Turma::getProfessorDaTurma() const {
+    std::cout << "Professor da Turma: " << professorDaTurma.getNome() << std::endl;
+}
+
+// Setters
+void Turma::setNomeDaTurma(const std::string& novaTurma) {
+    nomeDaTurma = novaTurma;
+}
+
+void Turma::setProfessorDaTurma(Professor novoProfessor) {
+    professorDaTurma = novoProfessor;
+}
+
+void Turma::setCapacidadeDaTurma(int capacidadeAlunos) {
+    capacidadeAlunos = capacidadeAlunos;
+}
