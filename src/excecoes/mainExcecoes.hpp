ced992430fbd7b6527f83fa3ec5a8051d37ba04d
Flavio <153334764+FlavioGermanoPinheiro@users.noreply.github.com>
2023-12-10 11:08:21 -0300

    Update mainExcecoes.hpp

--- src/excecoes/mainExcecoes.hpp
+++ src/excecoes/mainExcecoes.hpp
@@ -4,10 +4,27 @@
 #include <string>
 #include <stdexcept>
 
-class mainExcecoes : public std::runtime_error{
+/**
+ * @file mainExcecoes.hpp
+ * @brief Definição da classe mainExcecoes.
+ */
 
+/**
+ * @class mainExcecoes
+ * @brief Representa uma exceção relacionada ao código principal (main).
+ */
+class mainExcecoes : public std::runtime_error {
 public:
-    mainExcecoes(const std::string& msg) : std::runtime_error(msg) {}
-};
+    /**
+     * @brief Construtor parametrizado.
+     * @param msg Mensagem de erro associada à exceção.
+     */
+    mainExcecoes(const std::string& msg);
 
+    /**
+     * @brief Getter para obter a mensagem de erro.
+     * @return A mensagem de erro associada à exceção.
+     */
+    const char* what() const noexcept override;
+};
 #endif
