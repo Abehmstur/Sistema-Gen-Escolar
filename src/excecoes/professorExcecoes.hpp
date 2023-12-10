27111f98d9a7f69318500ea341b4cc25631b238f
Flavio <153334764+FlavioGermanoPinheiro@users.noreply.github.com>
2023-12-10 11:09:01 -0300

    Update professorExcecoes.hpp

--- src/excecoes/professorExcecoes.hpp
+++ src/excecoes/professorExcecoes.hpp
@@ -4,10 +4,28 @@
 #include <string>
 #include <stdexcept>
 
-class professorExcecoes : public std::runtime_error{
+/**
+ * @file professorExcecoes.hpp
+ * @brief Definição da classe professorExcecoes.
+ */
 
+/**
+ * @class professorExcecoes
+ * @brief Representa uma exceção relacionada à classe Professor.
+ */
+class professorExcecoes : public std::runtime_error {
 public:
-    professorExcecoes(const std::string& msg) : std::runtime_error(msg) {}
+    /**
+     * @brief Construtor parametrizado.
+     * @param msg Mensagem de erro associada à exceção.
+     */
+    professorExcecoes(const std::string& msg);
+
+    /**
+     * @brief Getter para obter a mensagem de erro.
+     * @return A mensagem de erro associada à exceção.
+     */
+    const char* what() const noexcept override;
 };
 
 #endif
