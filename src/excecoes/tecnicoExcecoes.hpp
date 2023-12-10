ab04116175e29fba966d39798ef140734c6fad71
Flavio <153334764+FlavioGermanoPinheiro@users.noreply.github.com>
2023-12-10 11:09:34 -0300

    Update tecnicoExcecoes.hpp

--- src/excecoes/tecnicoExcecoes.hpp
+++ src/excecoes/tecnicoExcecoes.hpp
@@ -4,10 +4,28 @@
 #include <string>
 #include <stdexcept>
 
-class tecnicoExcecoes : public std::runtime_error{
+/**
+ * @file tecnicoExcecoes.hpp
+ * @brief Definição da classe tecnicoExcecoes.
+ */
 
+/**
+ * @class tecnicoExcecoes
+ * @brief Representa uma exceção relacionada à classe TecnicoADM.
+ */
+class tecnicoExcecoes : public std::runtime_error {
 public:
-    tecnicoExcecoes(const std::string& msg) : std::runtime_error(msg) {}
+    /**
+     * @brief Construtor parametrizado.
+     * @param msg Mensagem de erro associada à exceção.
+     */
+    tecnicoExcecoes(const std::string& msg);
+
+    /**
+     * @brief Getter para obter a mensagem de erro.
+     * @return A mensagem de erro associada à exceção.
+     */
+    const char* what() const noexcept override;
 };
 
 #endif
